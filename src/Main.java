2ba853b29647dd009871c082fe5202fc09829188
Kenny Chavez <kennychaveez@gmail.com>
2019-10-23 14:38:28 -0600

    Added GridBagLayout Menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 public class Main{
 	JFrame mainFrame;
@@ -12,12 +14,47 @@ public class Main{
 		// Menú
 		mainFrame = new JFrame();
 		panel = new JPanel(); 
-        panel.setLayout(new FlowLayout());  
+        panel.setLayout(new GridBagLayout());
         lblMenu = new JLabel("Menú");
+        
+        // Constraints
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(lblMenu, gbc);
+        
         btnWareHouse = new JButton("Bodega");
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        panel.add(btnWareHouse, gbc);
+
         btnReports = new JButton("Reportes");
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        panel.add(btnReports, gbc);
+        
         btnKardex = new JButton("Kardex");
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 6;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        panel.add(btnKardex, gbc);
+        
         btnClose = new JButton("Salir");
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.gridx = 9;
+        gbc.gridy = 2;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        panel.add(btnClose, gbc);
         
         btnClose.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
@@ -25,23 +62,17 @@ public class Main{
 	        }
 	    });
         
-        panel.add(lblMenu);  
-        panel.add(btnWareHouse);
-        panel.add(btnReports);
-        panel.add(btnKardex);
-        panel.add(btnClose);
-        
         // Content
         
-        panelData = new JPanel(); 
-        panelData.setLayout(new FlowLayout());
-        lblTitle = new JLabel("Title");
+        // panelData = new JPanel(); 
+        // panelData.setLayout(new FlowLayout());
+        // lblTitle = new JLabel("Title");
         
-        btnWareHouse.addActionListener(new ActionListener(){
-        	public void actionPerformed(ActionEvent e){
+        // btnWareHouse.addActionListener(new ActionListener(){
+        // 	public void actionPerformed(ActionEvent e){
         		
-        	}
-        });
+        // 	}
+        // });
         
         mainFrame.add(panel);
         //mainFrame.add(panelData);
@@ -49,7 +80,6 @@ public class Main{
         mainFrame.setLocationRelativeTo(null);  
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         mainFrame.setVisible(true);
-        
 	}
 	
 	public static void main(String[] args) {
